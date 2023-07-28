1f11a5e5e679830139fbf9273f8c790f36ea7d89
sathish <sathishdevopstrainer@gmail.com>
2023-07-28 07:21:50 +0530

    Terraform: add ec2 terraform code

---
+++ provider.tf
@@ -0,0 +1,20 @@
+terraform {
+    required_version = "~> 1.5.1"
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 5.0"
+        }
+    }
+    backend "s3" {
+        bucket = "sathish-devops-bucket"
+        key = "terraform.tfstate"       
+        region = "ap-south-1"
+        dynamodb_table = "state-lock"
+    }
+}
+
+provider aws {
+    region = var.aws_region
+    profile = "default"
+}
